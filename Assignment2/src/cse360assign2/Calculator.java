315d2996c28511da258192ff418bc5ab2a85fc5b
alexbauman <39637295+alexbauman@users.noreply.github.com>
2019-02-22 13:10:40 -0700

    Initial commit of project.

---
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,85 @@
+/**	Author: Alexander Bauman
+ *  Class ID: 324
+ *  Assignment: 2
+ *  Description: This class creates	a simple calculator without an interface.
+ *  It has addition, subtraction, multiplication, and division functions,
+ *  as well as the ability to track the history of operations performed.
+ * 
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+
+	/**
+	 * This constructor initializes the total variable to 0
+	 */
+	
+	public Calculator() {
+		total = 0; // not needed - included for clarity
+	}
+
+	/**
+	 * This method returns the current total, as modified by the other methods.
+	 * 
+	 * @return 0	temporary value, to be changed
+	 */
+	
+	public int getTotal() {
+		return 0;
+	}
+
+	/**	
+	 * This method adds a user-inputted number to the current total.
+	 * 
+	 * @param value	 number inputted by the user
+	 */
+	
+	public void add(int value) {
+
+	}
+
+	/**	
+	 * This method subtracts a user-inputted number from the current total.
+	 * 
+	 * @param value	 number inputted by the user
+	 */
+	
+	public void subtract(int value) {
+
+	}
+
+	/**	
+	 * This method multiplies the current total by a user-inputted number.
+	 * 
+	 * @param value	 number inputted by the user
+	 */
+	
+	public void multiply(int value) {
+
+	}
+
+	/**	
+	 * This method divides the current total by a user-inputted number. It will
+	 * include a check to make the total 0 if the user enters a 0.
+	 * 
+	 * @param value	 number inputted by the user
+	 */
+	
+	public void divide(int value) {
+
+	}
+
+	/**
+	 * This method returns the history of operations performed on the total, in
+	 * the form of a string
+	 * 
+	 * @return ""  empty string, to be replaced
+	 */
+	
+	public String getHistory() {
+		return "";
+	}
+}
---
+++ Assignment2/src/cse360assign2/Main.java
@@ -0,0 +1,37 @@
+package cse360assign2;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		Scanner sc = new Scanner(System.in);
+		int op;
+		do {
+			System.out.println("Choose an operation:\n(1) Add\n(2) Subtract\n(3) Multiply\n(4) Divide\n(5) History\n(6) Total");
+			op = sc.nextInt();
+			switch(op) {
+			case 1: System.out.println("Enter an integer to add");
+					calc.add(sc.nextInt());
+					break;
+			case 2: System.out.println("Enter an integer to subtract");
+					calc.subtract(sc.nextInt());
+					break;
+			case 3: System.out.println("Enter an integer to multiply");
+					calc.multiply(sc.nextInt());
+					break;
+			case 4: System.out.println("Enter an integer to divide");
+					calc.divide(sc.nextInt());
+					break;
+			case 5: calc.getHistory();
+					break;
+			case 6: calc.getTotal();
+					break;
+			default: break;
+			}
+		} while (op != 0);
+
+	}
+
+}
